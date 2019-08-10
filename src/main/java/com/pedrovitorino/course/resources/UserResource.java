838bbc01d662e6b2a4a702703db1adbf3ffa8e09
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2019-08-10 14:37:00 -0300

    User entity and resource

---
+++ src/main/java/com/pedrovitorino/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.pedrovitorino.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrovitorino.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Pedro", "pedro@mai.com", "34999994444", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
