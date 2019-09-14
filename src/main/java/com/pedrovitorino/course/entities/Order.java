3ed93ce22d16e49410c8a312b87cc1f322418c64
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2019-09-14 08:53:23 -0300

    subotal & total methods

--- src/main/java/com/pedrovitorino/course/entities/Order.java
+++ src/main/java/com/pedrovitorino/course/entities/Order.java
@@ -100,6 +100,16 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		Double soma = 0.0;
+		
+		for(OrderItem x: items) {
+			soma = soma + x.getSubTotal();
+		}
+		
+		return soma;
+	}
 
 	@Override
 	public int hashCode() {
