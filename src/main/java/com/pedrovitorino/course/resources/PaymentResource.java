a19475046d5a510fc92003334e24a7cea9d8de5c
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2019-11-15 18:41:20 -0300

    payment put post endpoints

--- src/main/java/com/pedrovitorino/course/resources/PaymentResource.java
+++ src/main/java/com/pedrovitorino/course/resources/PaymentResource.java
@@ -1,5 +1,6 @@
 package com.pedrovitorino.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.pedrovitorino.course.dto.PaymentDTO;import com.pedrovitorino.course.services.PaymentService;
+import com.pedrovitorino.course.dto.PaymentDTO;
+import com.pedrovitorino.course.entities.Payment;
+import com.pedrovitorino.course.services.PaymentService;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -33,4 +40,19 @@ public class PaymentResource {
 		return ResponseEntity.ok().body(payment);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PostMapping
+	public ResponseEntity<PaymentDTO> insert(@RequestBody PaymentDTO obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<PaymentDTO> update(@PathVariable Long id, @RequestBody PaymentDTO obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
--- src/main/java/com/pedrovitorino/course/services/PaymentService.java
+++ src/main/java/com/pedrovitorino/course/services/PaymentService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pedrovitorino.course.dto.PaymentDTO;
+import com.pedrovitorino.course.entities.Order;
 import com.pedrovitorino.course.entities.Payment;
+import com.pedrovitorino.course.repositories.OrderRepository;
 import com.pedrovitorino.course.repositories.PaymentRepository;
 import com.pedrovitorino.course.services.exceptions.ResourceNotFoundException;
 
@@ -17,6 +22,8 @@ public class PaymentService {
 	
 	@Autowired
 	private PaymentRepository paymentRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 	
 	public List<PaymentDTO> findAll() {
 		List<Payment> list = paymentRepository.findAll();
@@ -27,5 +34,30 @@ public class PaymentService {
 		Optional<Payment> obj = paymentRepository.findById(id);
 		Payment entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new PaymentDTO(entity);
+	}
+	
+	@Transactional
+	public PaymentDTO insert(PaymentDTO obj) {
+		Order order = orderRepository.getOne(obj.getOrderId());
+		Payment entity = new Payment(null, obj.getMoment(), order);
+		order.setPayment(entity);
+		orderRepository.save(order);
+		return new PaymentDTO(order.getPayment());
+	}
+
+	@Transactional
+	public PaymentDTO update(Long id, PaymentDTO obj) {
+		try {
+			Payment entity = paymentRepository.getOne(id);
+			updateData(entity, obj);
+			entity = paymentRepository.save(entity);
+			return new PaymentDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Payment entity, PaymentDTO obj) {
+		entity.setMoment(obj.getMoment());
 	}
 }
